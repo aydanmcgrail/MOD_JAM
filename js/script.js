a7b354d859e07f584bc235a84fbb939d90f15ff9
aydanmcgrail <aydanmc2323@gmail.com>
2025-10-23 18:49:35 -0400

    not perfect but at least i have set up limits
    they can,t go out of bonds they come back but they stutter way too much.

--- js/script.js
+++ js/script.js
@@ -3,8 +3,9 @@
 let fly1 = undefined;
 let fly2 = undefined;
 let fly3 = undefined;
+let fly4 = undefined;
 
-let timer = 500;
+let interval = 60;
 
 function setup() {
   createCanvas(1600, 900);
@@ -14,14 +15,15 @@ function setup() {
   fly1 = createFly();
   fly2 = createFly();
   fly3 = createFly();
+  fly4 = createFly();
 }
 
 function createFly() {
   let fly = {
-    x: random(50, 600),
-    y: random(200, 500),
+    x: random(150, 800),
+    y: random(150, 700),
     size: 20,
-    speedX: 10,
+    speedX: 20,
     speedY: 10,
   };
   return fly;
@@ -30,20 +32,42 @@ function createFly() {
 function draw() {
   background("#87ceeb");
 
-  let s = millis() / 1000;
   moveFly(fly1);
   moveFly(fly2);
   moveFly(fly3);
+  moveFly(fly4);
 
   //draw the flies
   drawFly(fly1);
   drawFly(fly2);
   drawFly(fly3);
+  drawFly(fly4);
+
+  flyLimits(fly1);
+  flyLimits(fly2);
+  flyLimits(fly3);
+  flyLimits(fly4);
 }
 
 function moveFly(fly) {
   fly.x += random(-fly.speedX, fly.speedX);
   fly.y += random(-fly.speedY, fly.speedY);
+}
+
+function flyLimits(fly) {
+  if (fly.x > 1590) {
+    fly.x -= 22;
+  }
+
+  if (fly.x < 10) {
+    fly.x += 22;
+  }
+  if (fly.y > 790) {
+    fly.y -= 22;
+  }
+  if (fly.x < 10) {
+    fly.y += 22;
+  }
 }
 
 function drawFly(fly) {
