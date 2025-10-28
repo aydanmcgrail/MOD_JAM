df52807619dbe397dd329a7975398095e441c9d9
aydanmcgrail <aydanmc2323@gmail.com>
2025-10-28 16:46:25 -0700

    tongue is done
    The tongue now has a nice sine wave.
    other objective is to make the fly move more slower but keep that randomness.

--- js/script.js
+++ js/script.js
@@ -5,18 +5,18 @@ let fly2 = undefined;
 let fly3 = undefined;
 let fly4 = undefined;
 
-let easing = 0.04;
-let easing2 = 0.004;
 
+//this is for the easing
 let tongueX = 1570;
 let targetX = 1570;
 let min;
 let max;
-let click = 0;
+
+let easing = 0.008;
+let easing2 = 0.004;
 
 let life = 100; ///will be its hp 0 means the tongue will break.
-let wobbly = 100; //if wobbly= like over 100/extend too far then the tongue will go up and down.
-let moving = false;
+
 let wobble = 1;
 let wobbleTimer = 0; //each amount of set seconds will trigger opposite direction
 let wobbleLoop;
@@ -38,14 +38,18 @@ const frog = {
     velocity: 10,
     // Determines how the tongue moves each frame
     state: "idle", // State can be: idle, outbound, inbound
+    angle: 0,
   },
 };
 
 function setup() {
   createCanvas(1600, 900);
 
-  frameRate(50); // i think a lower framerate will be all around better
+  angleMode(DEGREES);
 
+  frameRate(50); // i think a lower framerate will be all around better does not matter actually
+
+  //this is for the easing
   tongueX = 1570;
   targetX = 1570;
   min = 50;
@@ -62,8 +66,9 @@ function createFly() {
     x: random(150, 800),
     y: random(150, 700),
     size: 20,
-    speedX: 20,
+    speedX: 10,
     speedY: 10,
+    velocity: { x: 0, y: 0 }
   };
   return fly;
 }
@@ -76,6 +81,7 @@ function draw() {
   text(wobbleTimer, width / 4.5, height / 4.5);
   text(wobbleLoop, width / 4.75, height / 4.75);
 
+
   moveFly(fly1);
   moveFly(fly2);
   moveFly(fly3);
@@ -85,8 +91,6 @@ function draw() {
   moveTongue();
   moveFrog();
   wobbleTongue();
-
-  keyPressed();
 
   //draw the flies
   drawFly(fly1);
@@ -103,24 +107,33 @@ function draw() {
   flyLimits(fly4);
 }
 
+function resetFly() {
+  fly.x;
+  fly.y;
+}
+
 function moveFly(fly) {
-  fly.x += random(-fly.speedX, fly.speedX);
-  fly.y += random(-fly.speedY, fly.speedY);
+  if (frameCount % 50 === 0) {
+    fly.velocity.x = random(-fly.speedX, fly.speedX);
+    fly.velocity.y = random(-fly.speedY, fly.speedY);
+  }
+  fly.x += fly.velocity.x;
+  fly.y += fly.velocity.y;
 }
 
 function flyLimits(fly) {
-  if (fly.x > 1590) {
-    fly.x -= 22;
+  if (fly.x > width) {
+    fly.x -= fly.speedX;
   }
 
-  if (fly.x < 10) {
-    fly.x += 22;
+  if (fly.x < 0) {
+    fly.x += fly.speedX;
   }
-  if (fly.y > 790) {
-    fly.y -= 22;
+  if (fly.y > height) {
+    fly.y -= fly.speedY
   }
-  if (fly.x < 10) {
-    fly.y += 22;
+  if (fly.x < 0) {
+    fly.y += fly.speedY;
   }
 }
 
@@ -128,6 +141,7 @@ function drawFly(fly) {
   push();
   noStroke();
   fill(255);
+  rectMode(CENTER);
   rect(fly.x, fly.y, fly.size);
   pop();
 }
@@ -152,31 +166,50 @@ function moveTongue() {
 function wobbleTongue() {
   //do not know how but this works. this can be a another to solve my problem
   // of wanting the tongue to move up and down past a certain widht
-  //frog.tongue.y = mouseY;
   //but that is not the way i wanted it to be. i want it to move by istelf out
-  //of the sheer weight of the longue stone tongue.
+  //of the sheer weight of the long stone tongue.
 
-  /*let cycleLength = 100;
-  let rawValue = frameCount % cycleLength;
-
-  loopValue = rawValue / (cycleLength - 1);
-*/
-
-  //frog.tongue.y = frog.body.y;
   //trigger an event when the tongue is extended too far
   if (tongueX <= 1300) {
     life -= 0.025; //this make somwehat of a second (minus one second)
-    moving = !moving;
     wobbleTimer += 0.025;
   } else {
     frog.tongue.y = frog.body.y;
     wobbleTimer = 0;
   }
-  if (moving) {
-    frog.tongue.y += wobbleTimer;
-  }
 
-  //if(frog.tongue.y )
+  frog.tongue.y = frog.body.y;
+  // How fast it wobble. 
+  // 10 degrees per frame at the centre
+  // 30 degrees per frame at the limit
+  const frequency = map(tongueX, width / 2, 100, 10, 30, true);
+  frog.tongue.angle += frequency;
+
+  // Calculate the sine, this will be a number between -1 and 1
+  const sine = sin(frog.tongue.angle);
+
+  // Calculate how big the motion is
+  // 0 wobble at the centre
+  // 10 pixels up and down at the limit
+  const amplitude = map(tongueX, width / 2, 100, 0, 10, true);
+
+  // How much to change y this frame
+  const offset = sine * amplitude;
+
+  // Draw the tongue tip
+  push();
+  fill("#6e6666ff");
+  noStroke();
+  ellipse(tongueX, frog.tongue.y + offset, frog.tongue.size);
+  pop();
+
+  // Draw the rest of the tongue
+  push();
+  stroke("#6e6666ff");
+  strokeWeight(frog.tongue.size);
+  line(tongueX, frog.tongue.y + offset, frog.body.x, frog.body.y);
+  pop();
+
 }
 
 function moveFrog() {
@@ -187,20 +220,6 @@ function moveFrog() {
 }
 
 function drawFrog() {
-  // Draw the tongue tip
-  push();
-  fill("#6e6666ff");
-  noStroke();
-  ellipse(tongueX, frog.tongue.y, frog.tongue.size);
-  pop();
-
-  // Draw the rest of the tongue
-  push();
-  stroke("#6e6666ff");
-  strokeWeight(frog.tongue.size);
-  line(tongueX, frog.tongue.y, frog.body.x, frog.body.y);
-  pop();
-
   // Draw the frog's body
   push();
   fill("#00ff00");
@@ -225,9 +244,4 @@ function checkTongueFlyOverlap() {
   }
 }
 
-function keyPressed() {
-  // Prevent default browser behavior for arrow keys
-  if (keyCode === 65 || keyCode === 68) {
-    return false;
-  }
-}
+
