bea2d8fe3dbf3757283d54c1f0fe097fd0b70b95
aydanmcgrail <aydanmc2323@gmail.com>
2025-11-03 20:05:19 -0500

    close to finished
    need to upgrade some parts

--- js/script.js
+++ js/script.js
@@ -1,32 +1,44 @@
 "use strict";
 
+//reset function////
+let xPos = 62;
+let yPos = 934;
+let clickWidth = 52;
+let clickHeight = 52;
+
+//for the flies creation///
 let fly1 = undefined;
 let fly2 = undefined;
 let fly3 = undefined;
 let fly4 = undefined;
 
+////for health points
 let colorGreen = "#00ff00";
 let colorRed = "#ff0000";
-let flyState = [0, 2, 3];
-let death = flyState[0];
-let flyHit = true; //flyState[1];
-let flyAlive = flyState[2];
 
-//this is for the easing
+let life = 100;
+let minLife = 0;
+let maxLife = 100;
+let gameOverY = -10;
+let minLifeY = -500;
+let maxLifeY = 550;
+let tongueSecurity;
+
+//this is for the easing and the tongue moving
 let tongueX = 1450;
 let targetX = 1450;
 let min;
 let max;
-
 let easing = 0.01;
 let easing2 = 0.004;
 
-let life = 100; ///will be its hp 0 means the tongue will break. GAME OVER
+/////////////////////the effects the tongue is subject to//////////////////////
+/////////////////////the effects the tongue is subject to//////////////////////
+/////////////////////the effects the tongue is subject to//////////////////////
 
-let wobble = 1;
-let wobbleTimer = 0; //each amount of set seconds will trigger opposite direction
-let wobbleLoop = 100;
+let amplitudeTongueNumber = 40; //starts at 30 will go lower with stone transformation.
 
+/////////////////////////////////frog///////////////////////////////
 const frog = {
   // The frog's body has a position and size
   body: {
@@ -41,23 +53,31 @@ const frog = {
     x: undefined,
     y: 400,
     size: 70,
-    speed: 10,
+    speed: 10, //this might do nothing
     velocity: 10,
-    // Determines how the tongue moves each frame
-    state: "idle", // State can be: idle, outbound, inbound
-    angle: 0,
+    tongueHitFly: false, // State can be: idle, outbound, inbound
+    angle: 0, //for the sine wave
+    fill: { r: 150, g: 200, b: 60 },
   },
 };
 
+///////////////////////////////////images and sounds//////////////////////////////
+///////////////////////////////////images and sounds//////////////////////////////
+///////////////////////////////////images and sounds//////////////////////////////
 let img1;
 let img2;
 let img3;
 let img4;
 let img5;
+let img6;
+let img7;
+let img8;
+
 let opacityImg1 = 0; //i will up the value so img1 will become visible
 let opacityImg2 = 255; // i will lower the value so img1 will become transparent
 let opacityImg5 = 255;
-let opacityFly = 255;
+let opacityStone = 255; //this will be put on both images of frog and they will
+//gradually disappear to show the stone version underneath.
 
 function preload() {
   img1 = loadImage("./assets/images/frog.png");
@@ -65,8 +85,14 @@ function preload() {
   img3 = loadImage("./assets/images/floor.png");
   img4 = loadImage("./assets/images/mouche.png");
   img5 = loadImage("./assets/images/frogstone.png");
+  img6 = loadImage("./assets/images/infobar.png");
+  img7 = loadImage("./assets/images/moucheroche.png");
+  img8 = loadImage("./assets/images/frogidlestone.png");
 }
 
+/////////////////////////////////////setup///////////////////////////////////////////
+/////////////////////////////////////setup///////////////////////////////////////////
+/////////////////////////////////////setup///////////////////////////////////////////
 function setup() {
   createCanvas(1600, 1000);
 
@@ -77,7 +103,7 @@ function setup() {
 
   angleMode(DEGREES);
 
-  frameRate(50); // i think a lower framerate will be all around better does not matter actually
+  //frameRate(50); // i think a lower framerate will be all around better does not matter actually
 
   fly1 = createFly();
   fly2 = createFly();
@@ -90,26 +116,26 @@ function createFly() {
     x: random(150, 800),
     y: random(150, 700),
     size: 100,
-    speedX: 3,
-    speedY: 3,
+    speedX: 6,
+    speedY: 6,
     velocity: { x: 0, y: 0 },
-    squareRight: "#00ff00",
-    squareCenter: "#00ff00",
-    squareLeft: "#00ff00",
+    squareRight: "#7fe97fff",
+    squareCenter: "#7fe97fff",
+    squareLeft: "#7fe97fff",
+    opacityFly: 255,
+    flyHit: 0,
   };
   return fly;
 }
 
+//////////////////////////////////////draw///////////////////////////////////////////
+//////////////////////////////////////draw///////////////////////////////////////////
+//////////////////////////////////////draw///////////////////////////////////////////
 function draw() {
-  background("#87ceeb");
-
-  image(img3, 0, 0);
-
-  //checkLife();
-  text(life, width / 4, height / 4);
-  text(wobbleTimer, width / 4.5, height / 4.5);
-  text(flyHit, width / 6, height / 3);
-  text(opacityImg2, width / 2.75, height / 2.75);
+  background("#272727ff");
+  push();
+  image(img3, 0, 0); //the floor
+  pop();
 
   moveFly(fly1);
   moveFly(fly2);
@@ -118,6 +144,7 @@ function draw() {
 
   //draw the frog
   drawFrog(); //frog is behind the tongue since it will come out of mouth
+  drawFrogStoned(); //this stone frog is behind the not stone looking frog pngs
   drawFrogOpen();
 
   //move the tongue
@@ -139,9 +166,20 @@ function draw() {
   checkOverlap(frog, fly4);
 
   push();
-  fill(0);
-  rect(0, 900, 1600, 100);
+  image(img6, 0, 853);
   pop();
+
+  push();
+  fill("red");
+  ellipse(xPos, yPos, clickWidth, clickHeight);
+  pop();
+
+  textSize(50);
+  textStyle(BOLD);
+  textFont("Courier New");
+  text("R", 47, 949);
+
+  checkLife();
 }
 
 function moveFly(fly) {
@@ -150,9 +188,13 @@ function moveFly(fly) {
     fly.velocity.y = random(-fly.speedY, fly.speedY);
   }
   //here i do not want flies to get too close to frog
-  if (fly.x > 1300 || fly.x < 100) {
+  if (fly.x > 1300) {
     fly.velocity.x = fly.velocity.x * -1;
   }
+  if (fly.x < 100) {
+    fly.velocity.x = fly.velocity.x * -1;
+  }
+
   if (fly.y > 800 || fly.y < 100) {
     fly.velocity.y = fly.velocity.y * -1;
   }
@@ -162,13 +204,23 @@ function moveFly(fly) {
 }
 
 function drawFly(fly) {
-  push();
+  //push();
   noStroke();
   fill(255);
   rectMode(CENTER);
-  ellipse(fly.x, fly.y, fly.size);
-  tint(255, opacityFly);
+  ellipse(fly.x, fly.y, fly.size); //the collision is with the ellipse not the png of the flies
+
+  image(img7, fly.x - 90, fly.y - 100);
+
+  push();
+  tint(255, fly.opacityFly);
+  //the normal fly png sits on top. if they turn to stone the
+  // opacity goes to 0 and shows the png behind
   image(img4, fly.x - 90, fly.y - 100);
+  pop();
+
+  textSize(24);
+  text(fly.flyHit, fly.x + 110, fly.y + 130);
 
   push();
   fill(fly.squareRight);
@@ -184,13 +236,13 @@ function drawFly(fly) {
   fill(fly.squareLeft);
   rect(fly.x - 20, fly.y - 110, 15);
   pop();
-
-  pop();
 }
 
 function moveTongue() {
   // Tongue matches the frog's y
-  //frog.tongue.y = frog.body.y;
+  frog.tongue.y = frog.body.y;
+
+  //targetX = mouseX;  ////for moving faster with mouse
 
   if (keyIsPressed) {
     if (keyCode === 65) {
@@ -199,25 +251,51 @@ function moveTongue() {
       targetX += 5;
     }
   }
+
   targetX = targetX + 2;
   targetX = constrain(targetX, min, max);
 
   tongueX += (targetX - tongueX) * easing;
 }
 
-function wobbleTongue() {
-  //do not know how but this works. this can be a another to solve my problem
-  // of wanting the tongue to move up and down past a certain widht
-  //but that is not the way i wanted it to be. i want it to move by istelf out
-  //of the sheer weight of the long stone tongue.
+function checkLife() {
+  push();
+  textSize(42);
+  textStyle(BOLD);
+  textFont("Courier New");
+  fill(colorGreen);
+  text(life, 1496, 948); ///life=100,will be its hp 0 means the tongue will break. GAME OVER
+  fill(colorRed);
+  textSize(275);
+  stroke(0);
+  strokeWeight(8);
+  textStyle(ITALIC);
+  text("GAME OVER", 20, gameOverY);
+  pop();
 
+  life = constrain(life, minLife, maxLife);
+
+  textSize(32);
+  text(tongueX, 200, 100);
+
+  //tongueX = tongueSecurity;
+
+  if (life <= 0) {
+    gameOverY += 1;
+  } else {
+    gameOverY;
+  }
+  gameOverY = constrain(gameOverY, minLifeY, maxLifeY);
+}
+
+function wobbleTongue() {
   //trigger an event when the tongue is extended too far
   if (tongueX <= 1300) {
-    life -= 0.025; //this make somwehat of a second (minus one second)
-    wobbleTimer += 0.025;
+    life -= 0.25; //this make somwehat of a second (minus one second)
+  } else if (tongueX <= 600) {
+    life -= 0.5;
   } else {
     frog.tongue.y = frog.body.y;
-    wobbleTimer = 0;
   }
 
   //trigger a new image when the tongue is resting at the base position.(x=1500px)
@@ -225,22 +303,21 @@ function wobbleTongue() {
     //close to rest position,0 tongue extension (1500px) && img1 is showing(true)
     opacityImg2 -= 25;
     opacityImg1 = 255;
-    opacityImg5 = 0;
   } else {
     opacityImg2 = 255;
     opacityImg1 = 0;
-    opacityImg5 = 0;
-    //img1.hide();
-    // img2.show();
+  }
+  if (life <= 0) {
+    opacityImg2 = 0;
+    opacityImg1 = 0;
   }
 
-  frog.tongue.y = frog.body.y;
   ///////////////////////////////sine wave ///////////////////////////////////
   ///////////////////////////////sine wave ///////////////////////////////////
   // How fast it wobble.
   // 10 degrees per frame at the centre
   // 30 degrees per frame at the limit
-  const frequency = map(tongueX, width / 2, 100, 10, 30, true);
+  let frequency = map(tongueX, width / 2, 200, 10, 30, true);
   frog.tongue.angle += frequency;
 
   // Calculate the sine, this will be a number between -1 and 1
@@ -249,7 +326,7 @@ function wobbleTongue() {
   // Calculate how big the motion is
   // 0 wobble at the centre
   // 10 pixels up and down at the limit
-  const amplitude = map(tongueX, width / 2, 100, 0, 10, true);
+  let amplitude = map(tongueX, width / 2, 200, 0, amplitudeTongueNumber, true);
 
   // How much to change y this frame
   const offset = sine * amplitude;
@@ -259,14 +336,14 @@ function wobbleTongue() {
 
   // Draw the tongue tip
   push();
-  fill("#79484eff");
+  fill(frog.tongue.fill);
   noStroke();
   ellipse(tongueX, frog.tongue.y + offset, frog.tongue.size);
   pop();
 
   // Draw the rest of the tongue
   push();
-  stroke("#79484eff");
+  stroke(frog.tongue.fill);
   strokeWeight(frog.tongue.size);
   line(tongueX, frog.tongue.y + offset, frog.body.x, frog.body.y);
   pop();
@@ -274,10 +351,15 @@ function wobbleTongue() {
 
 function moveFrog() {
   //frog.body.y = mouseY;
-  let targetY = mouseY;
-  let dx = targetY - frog.body.y;
-  frog.body.y += dx * easing2;
-  frog.body.y = constrain(frog.body.y, frog.body.minY, frog.body.maxY);
+
+  if (life <= 0) {
+    frog.body.y;
+  } else {
+    let targetY = mouseY;
+    let dx = targetY - frog.body.y;
+    frog.body.y += dx * easing2;
+    frog.body.y = constrain(frog.body.y, frog.body.minY, frog.body.maxY);
+  }
 }
 
 function drawFrog() {
@@ -321,8 +403,52 @@ function checkOverlap(frog, fly) {
   // Check if it's an overlap
   const hit = d < frog.tongue.size / 2 + fly.size / 2;
   if (hit) {
-    fly.squareRight = "#ff0000";
-    flyHit = false;
-    fly.velocity.x = -4.5;
-  } else flyHit = true;
+    fly.squareRight = "#d13c3cff";
+    fly.flyHit += 0.05; //for me to see if collision works and it's value
+    //frog.tongue.tongueHitFly = true;
+    fly.velocity.x = -3; //it goes opposite from the tongue
+    fly.opacityFly = 150;
+    fly.speedX = 3;
+    fly.speedY = 3;
+    fly.state = "inbound";
+    targetX += 10;
+  } else {
+    frog.tongue.tongueHitFly = false;
+    targetX = targetX;
+    //fly.flyTouch = false;
+  }
+
+  if (hit && fly.flyHit > 2.5) {
+    fly.squareCenter = "#d13c3cff";
+    //frog.tongue.tongueHitFly = true; //the tongue is hitting the fly
+    fly.velocity.x = -3; //it goes opposite from the tongue
+    fly.opacityFly = 50; //turns the fly more into stone
+    fly.speedX = 2; //since it turned into stone it is slower
+    fly.speedY = 2; //since it turned into stone it is slower
+    targetX += 10;
+  } else {
+    frog.tongue.tongueHitFly = false; //the tongue is not in collision with any flies
+    targetX = targetX;
+  }
+
+  if (hit && fly.flyHit > 3.5) {
+    fly.squareLeft = "#d13c3cff";
+    //frog.tongue.tongueHitFly = true; //the tongue is hitting the fly
+    fly.velocity.x = -0.25; //it goes opposite from the tongue
+    fly.opacityFly = 0; //turns the fly more into stone
+    fly.speedX = 0; //fully turned to stone so does not move
+    fly.speedY = 0; //fully turned to stone so does not move
+    targetX += 10;
+  } else {
+    frog.tongue.tongueHitFly = false; //the tongue is not in collision with any flies
+    targetX = targetX;
+  }
+}
+
+function keyPressed() {
+  if (keyIsPressed) {
+    if (keyCode === 82) {
+      location.reload();
+    }
+  }
 }
